fccabe5e227c84037c57f9eb51921f96629794e9
Manish Kumar Verma <157243115+VERMAMANISHKUMAR@users.noreply.github.com>
2024-12-02 01:47:55 +0530

    same changes

--- Week-01/3. Handling Multiple Request/Server.js
+++ Week-01/3. Handling Multiple Request/Server.js
@@ -2,10 +2,11 @@
  const https=require('https');
  //02 Create the Server
  const server = https.createServer((req,res)=>{
+ 
  res.end('Welcome to Node Js Server');
  })
  //03 Create the Server Ports
- const port =process.env.PORT||3200;
+ const port =process.env.PORT||3000;
   server.listen(port,()=>{
           console.log(`Server is running at https://localhost:${port}`);
   })
