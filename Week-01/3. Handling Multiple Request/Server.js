5319c895c117ff6edcec63575e3fe755bd0858ec
Manish Kumar Verma <157243115+VERMAMANISHKUMAR@users.noreply.github.com>
2024-12-04 01:38:22 +0530

    multipal request on server

--- Week-01/3. Handling Multiple Request/Server.js
+++ Week-01/3. Handling Multiple Request/Server.js
@@ -1,12 +1,32 @@
-// 01 import the https library/modules
- const https=require('https');
- //02 Create the Server
- const server = https.createServer((req,res)=>{
+// // 01 import the https library/modules
+//  const https=require('https');
+//  //02 Create the Server
+//  const server = https.createServer((req,res)=>{
  
- res.end('Welcome to Node Js Server');
- })
- //03 Create the Server Ports
- const port =process.env.PORT||3000;
-  server.listen(port,()=>{
-          console.log(`Server is running at https://localhost:${port}`);
-  })
+//  res.end('Welcome to Node Js Server');
+//  })
+//  //03 Create the Server Ports
+//  const port =process.env.PORT||3000;
+//   server.listen(port,()=>{
+//           console.log(`Server is running at https://localhost:${port}`);
+//   })
+// -------------------
+// 01 import the http library/module
+const http=require('http');
+
+// 02 Create Server
+const server=http.createServer((req,res)=>{
+    console.log(req.url);
+  res.write('Welcome to my Application ')//Add the content
+  if(req.url=='/Product'){
+  res.end('You are viewing Product Page');
+  }
+  else if (req.url=='/Users'){
+                res.end('You are viewing Users Page');
+        }
+
+});
+// 03S Specify Server Port
+server.listen(3100,()=>{
+  console.log('Server is running on port 3100');
+});
