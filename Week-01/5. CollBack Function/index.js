6b474c33bc2cc3774c60d8d0c2b30adde0bffa35
Manish Kumar Verma <157243115+VERMAMANISHKUMAR@users.noreply.github.com>
2024-12-11 12:31:05 +0530

    commit the code

--- Week-01/5. CollBack Function/index.js
+++ Week-01/5. CollBack Function/index.js
@@ -8,7 +8,7 @@ const add = function(a,b,callback)
 {
         
           var result=a+b;
-          console.log("this is my main function = "+result)
-          callback()
+          console.log("this is my main function = "+result)//main function work is complete
+          callback()//after run the seconde function
 }
 add(10,50,callback)
